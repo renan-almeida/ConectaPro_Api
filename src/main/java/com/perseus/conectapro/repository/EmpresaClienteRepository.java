3fae9dc219532e22ca744e8ab6f28694ab99ae8b
Danillo <danillo.lacerda77@gmail.com>
2025-04-04 20:26:43 -0300

    teste4

--- src/main/java/com/perseus/conectapro/repository/EmpresaClienteRepository.java
+++ src/main/java/com/perseus/conectapro/repository/EmpresaClienteRepository.java
@@ -1,12 +1,12 @@
 package com.perseus.conectapro.Repository;
 
 import com.perseus.conectapro.Entity.EmpresaCliente;
+import com.perseus.conectapro.Entity.Prestador;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EmpresaClienteRepository extends JpaRepository<EmpresaCliente, Integer> {
-    List<EmpresaCliente> findByNome(String name);
+    List<EmpresaCliente> findBySegmentoContaining(String segmento);
+
 }
