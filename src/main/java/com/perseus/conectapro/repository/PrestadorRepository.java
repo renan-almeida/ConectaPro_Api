fc49bb13bfc02fd9a1567574c685fc41ee6d594b
Danillo <danillo.lacerda77@gmail.com>
2025-04-04 20:24:02 -0300

    teste3

--- src/main/java/com/perseus/conectapro/repository/PrestadorRepository.java
+++ src/main/java/com/perseus/conectapro/repository/PrestadorRepository.java
@@ -3,9 +3,11 @@ package com.perseus.conectapro.Repository;
 import com.perseus.conectapro.Entity.Prestador;
 import com.perseus.conectapro.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PrestadorRepository extends JpaRepository<Prestador, Integer> {
 
     List<Prestador> findByNome(String name);
