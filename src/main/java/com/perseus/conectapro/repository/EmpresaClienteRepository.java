5d01f2a2cc9757c3cf79e78927d9b65b4447c3f1
Danillo <danillo.lacerda77@gmail.com>
2025-04-04 20:24:42 -0300

    teste3

--- src/main/java/com/perseus/conectapro/repository/EmpresaClienteRepository.java
+++ src/main/java/com/perseus/conectapro/repository/EmpresaClienteRepository.java
@@ -1,7 +1,6 @@
 package com.perseus.conectapro.Repository;
 
 import com.perseus.conectapro.Entity.EmpresaCliente;
-import com.perseus.conectapro.Entity.Prestador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/perseus/conectapro/repository/PrestadorRepository.java
+++ src/main/java/com/perseus/conectapro/repository/PrestadorRepository.java
@@ -1,7 +1,6 @@
 package com.perseus.conectapro.Repository;
 
 import com.perseus.conectapro.Entity.Prestador;
-import com.perseus.conectapro.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
